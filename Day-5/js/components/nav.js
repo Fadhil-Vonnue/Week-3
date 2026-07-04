8c4d82dd517c08bcb16919e66f98c86dd5c56bfb
Mohammad Fadhil Nouhar A P <mohammad.fadhil@vonnue.com>
2026-07-04 17:51:53 +0530

    feat: Polished and Deployed Project
    - Zero console errors on every page.
    - ESLint formatted
    - Tested all features at 320px
    - Tested keyboard only navigation

--- Day-5/js/components/nav.js
+++ Day-5/js/components/nav.js
@@ -1,48 +1,48 @@
 export function openHam(button, content) {
-  const isOpen = content.classList.toggle("isToggled");
-  if (isOpen) {
-    content.style.maxHeight = "500px";
-    content.style.padding="20px"
-    content.ariaExpanded = true;
-    document.body.style.overflow = "hidden";
-  } else {
-    content.style.maxHeight = "0px";
-    content.style.padding="0px"
-    content.ariaExpanded = false;
-    content.style.overflow="hidden"
-    document.body.style.overflow = "visible";
-  }
+    const isOpen = content.classList.toggle("isToggled");
+    if (isOpen) {
+        content.style.maxHeight = "500px";
+        content.style.padding = "20px";
+        content.ariaExpanded = true;
+        document.body.style.overflow = "hidden";
+    } else {
+        content.style.maxHeight = "0px";
+        content.style.padding = "0px";
+        content.ariaExpanded = false;
+        content.style.overflow = "hidden";
+        document.body.style.overflow = "visible";
+    }
 }
 
 export function openHam1(e) {
-  openHam(e.currentTarget, document.querySelector(".drawer"));
+    openHam(e.currentTarget, document.querySelector(".drawer"));
 }
 
 const drawer = document.querySelector(".drawer");
 
 export function trapFocus(element) {
-  const focusableSelectors = "a[href]";
-  const focusableElements = element.querySelectorAll(focusableSelectors);
+    const focusableSelectors = "a[href]";
+    const focusableElements = element.querySelectorAll(focusableSelectors);
 
-  const firstFocusable = focusableElements[0];
-  const lastFocusable = focusableElements[focusableElements.length - 1];
+    const firstFocusable = focusableElements[0];
+    const lastFocusable = focusableElements[focusableElements.length - 1];
 
-  if (firstFocusable) firstFocusable.focus();
+    if (firstFocusable) firstFocusable.focus();
 
-  element.addEventListener("keydown", function (e) {
-    if (e.key !== "Tab") return;
+    element.addEventListener("keydown", function (e) {
+        if (e.key !== "Tab") return;
 
-    if (e.shiftKey) {
-      if (document.activeElement === firstFocusable) {
-        lastFocusable.focus();
-        e.preventDefault();
-      }
-    } else {
-      if (document.activeElement === lastFocusable) {
-        firstFocusable.focus();
-        e.preventDefault();
-      }
-    }
-  });
+        if (e.shiftKey) {
+            if (document.activeElement === firstFocusable) {
+                lastFocusable.focus();
+                e.preventDefault();
+            }
+        } else {
+            if (document.activeElement === lastFocusable) {
+                firstFocusable.focus();
+                e.preventDefault();
+            }
+        }
+    });
 }
 trapFocus(drawer);
--- Day-5/js/util.js
+++ Day-5/js/util.js
@@ -1,57 +1,56 @@
 export async function fetchJSON(url, options) {
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error("Error while fetching");
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error("Error while fetching");
+        }
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        return Promise.reject(err);
     }
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    return Promise.reject(err);
-  }
 }
 
 export function debounce(fn, time = 300) {
-  setTimeout(fn, time);
+    setTimeout(fn, time);
 }
 
 export function showToast(err, duration, type = "error") {
-  const check=document.querySelector(".toaster")
-  if(check){
-    check.remove()
-  }
-  const toaster = document.createElement("div");
-  toaster.classList.add("toaster");
-  const toasterWarning = document.createElement("div");
-  toasterWarning.classList.add("toastWarning");
-  const toasterErrorMessage = document.createElement("div");
-  toasterErrorMessage.classList.add("toastErrormessage");
-  const toastProgress = document.createElement("div");
-  toastProgress.classList.add("toastProgress");
-  const CSSstyle = document.createElement("style");
-  let toastColor="#ffd7d3"
-  let toastProgressBar="red"
-  if (type.toLowerCase() == "error") {
-    toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=8122&format=png&color=FF0000"/>`;
-  }
-  if(type.toLowerCase()==="success"){
-    toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=63312&format=png&color=000000"/>`;
-    toastColor="#8dddae;"
-    toastProgressBar="green"
-  }
-  if(type.toLowerCase()==="info"){
-    toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=63308&format=png&color=000000"/>`;
-    toastColor="#7db5f5;"
-    toastProgressBar="blue"
-  }
-  if(type.toLowerCase()==="warning"){
-    toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=EggHJUeUuU6C&format=png&color=000000"/>`;
-    toastColor="#f5d97d;"
-    toastProgressBar="#ffc400;"
-  }
-  https://img.icons8.com/?size=100&id=12116&format=png&color=000000
-  toasterErrorMessage.textContent = err.message;
-  CSSstyle.textContent += `*{
+    const check = document.querySelector(".toaster");
+    if (check) {
+        check.remove();
+    }
+    const toaster = document.createElement("div");
+    toaster.classList.add("toaster");
+    const toasterWarning = document.createElement("div");
+    toasterWarning.classList.add("toastWarning");
+    const toasterErrorMessage = document.createElement("div");
+    toasterErrorMessage.classList.add("toastErrormessage");
+    const toastProgress = document.createElement("div");
+    toastProgress.classList.add("toastProgress");
+    const CSSstyle = document.createElement("style");
+    let toastColor = "#ffd7d3";
+    let toastProgressBar = "red";
+    if (type.toLowerCase() == "error") {
+        toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=8122&format=png&color=FF0000"/>`;
+    }
+    if (type.toLowerCase() === "success") {
+        toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=63312&format=png&color=000000"/>`;
+        toastColor = "#8dddae;";
+        toastProgressBar = "green";
+    }
+    if (type.toLowerCase() === "info") {
+        toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=63308&format=png&color=000000"/>`;
+        toastColor = "#7db5f5;";
+        toastProgressBar = "blue";
+    }
+    if (type.toLowerCase() === "warning") {
+        toasterWarning.innerHTML = `<img width="20" height="20" src="https://img.icons8.com/?size=100&id=EggHJUeUuU6C&format=png&color=000000"/>`;
+        toastColor = "#f5d97d;";
+        toastProgressBar = "#ffc400;";
+    }
+    https: toasterErrorMessage.textContent = err.message;
+    CSSstyle.textContent += `*{
       box-sizing: border-box;
      }
   .animateToaster{
@@ -145,23 +144,24 @@ export function showToast(err, duration, type = "error") {
         width: 0%;
       }
     }`;
-  document.head.appendChild(CSSstyle);
-  toaster.appendChild(toasterWarning);
-  toaster.appendChild(toasterErrorMessage);
-  toaster.appendChild(toastProgress);
-  document.body.prepend(toaster);
+    document.head.appendChild(CSSstyle);
+    toaster.appendChild(toasterWarning);
+    toaster.appendChild(toasterErrorMessage);
+    toaster.appendChild(toastProgress);
+    document.body.prepend(toaster);
 }
-export function createRetryButton(fn){
-  const div=document.createElement("div")
-  div.classList.add("retryButton")
-  const imageElement=document.createElement("img")
-  imageElement.src=`https://img.icons8.com/?size=100&id=11684&format=png&color=FFFFFF`
-  imageElement.width="40"
-  imageElement.alt=""
-  div.appendChild(imageElement)
-  imageElement.addEventListener("click",(e)=>{fn()
-    div.classList.add("hidden")
-    div.remove()
-  })
-  return div
+export function createRetryButton(fn) {
+    const div = document.createElement("div");
+    div.classList.add("retryButton");
+    const imageElement = document.createElement("img");
+    imageElement.src = `https://img.icons8.com/?size=100&id=11684&format=png&color=FFFFFF`;
+    imageElement.width = "40";
+    imageElement.alt = "";
+    div.appendChild(imageElement);
+    imageElement.addEventListener("click", (e) => {
+        fn();
+        div.classList.add("hidden");
+        div.remove();
+    });
+    return div;
 }
